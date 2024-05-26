7e51faa5a1b276e67ca73d1ad96f8820464a3772
felipelvgr <felipeglavarde@icloud.com>
2024-05-26 17:29:30 -0300

    Initial commit

---
+++ Calculate the width of the wall.py
@@ -0,0 +1,13 @@
+largura = float(input("Largura da Parede:"))
+altura = float(input("Altura da Parede:"))
+area = largura * altura
+print("Sua parede tem a dimensao de {}x{} e sua are e de {}m".format(largura, altura, area))
+tinta = area / 2
+print("Para pintar essa parede, voce precisara de {}L de tinta".format(tinta))
+
+
+
+
+
+
+
